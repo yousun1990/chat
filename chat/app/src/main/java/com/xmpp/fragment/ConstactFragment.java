c22e89a1a4c6d6d9be8f280e616c78547a088e19
unknown <yousun1990@gmail.com>
2019-12-17 16:10:31 +0800

    chat 的 demo提交

---
+++ chat/app/src/main/java/com/xmpp/fragment/ConstactFragment.java
@@ -0,0 +1,131 @@
+package com.xmpp.fragment;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.RosterEntry;
+import org.jivesoftware.smack.RosterGroup;
+import org.jivesoftware.smack.XMPPConnection;
+
+import com.xmpp.MyApplication;
+import com.xmpp.R;
+import com.xmpp.activity.AddFriendActivity;
+import com.xmpp.activity.ChatActivity;
+import com.xmpp.adapter.ConstactAdapter;
+import com.xmpp.bean.Child;
+import com.xmpp.bean.Group;
+import com.xmpp.view.IphoneTreeView;
+import com.xmpp.view.TitleBarView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+
+public class ConstactFragment extends Fragment {
+	private Context mContext;
+	private View mBaseView;
+	private TitleBarView mTitleBarView;
+	private IphoneTreeView mIphoneTreeView;
+	private ConstactAdapter mExpAdapter;
+	private List<Group> listGroup;
+	
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+		mContext = getActivity();
+		mBaseView = inflater.inflate(R.layout.fragment_constact_father, null);
+		findView();
+		return mBaseView;
+	}
+
+	private void findView() {
+		mTitleBarView=(TitleBarView) mBaseView.findViewById(R.id.title_bar);
+		mTitleBarView.setCommonTitle(View.GONE, View.VISIBLE, View.GONE, View.VISIBLE);
+		mTitleBarView.setTitleText(R.string.constacts);//标题
+		mTitleBarView.setTitleRight("添加");//右按钮-添加好友
+		mTitleBarView.setBtnRightOnclickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent=new Intent(mContext, AddFriendActivity.class);
+				startActivity(intent);
+			}
+		});
+		mIphoneTreeView = (IphoneTreeView) mBaseView.findViewById(R.id.iphone_tree_view);
+		mIphoneTreeView.setHeaderView(LayoutInflater.from(mContext).inflate(R.layout.fragment_constact_head_view, mIphoneTreeView, false));
+		mIphoneTreeView.setGroupIndicator(null);
+		mIphoneTreeView.setOnChildClickListener(new OnChildClickListener() {
+			@Override
+			public boolean onChildClick(ExpandableListView arg0, View arg1, int arg2,int arg3, long arg4) {
+				Intent intent =new Intent(mContext, ChatActivity.class);
+				intent.putExtra("from", listGroup.get(arg2).getChildList().get(arg3).getUsername());
+				startActivity(intent);
+				return true;
+			}
+		});
+		if(MyApplication.xmppConnection==null){
+			new Handler().postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					initData();
+				}
+			}, 1000);
+		}else{
+			initData();
+		}
+	}
+	
+	/**
+	 * 加载数据
+	 */
+	void initData(){
+		findFriends();
+		if(listGroup.size()<=0){
+			mIphoneTreeView.setVisibility(View.GONE);
+			return;
+		}
+		mExpAdapter = new ConstactAdapter(mContext, listGroup, mIphoneTreeView);
+		mIphoneTreeView.setAdapter(mExpAdapter);
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+	}
+	
+	public void findFriends() {
+		try {
+			listGroup=new ArrayList<Group>();
+			XMPPConnection conn = MyApplication.xmppConnection;
+			Roster roster = conn.getRoster();
+			Collection<RosterGroup> groups = roster.getGroups();
+			for (RosterGroup group : groups) {
+				Group mygroup=new Group();
+				mygroup.setGroupName(group.getName());
+				Collection<RosterEntry> entries = group.getEntries();
+				List<Child> childList=new ArrayList<Child>();
+				for (RosterEntry entry : entries) {
+					if(entry.getType().name().equals("both")){
+						Child child=new Child();
+						child.setUsername(entry.getUser().split("@")[0]);
+						childList.add(child);
+					}
+				}
+				mygroup.setChildList(childList);
+				listGroup.add(mygroup);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ chat/app/src/main/java/com/xmpp/listener/CheckConnectionListener.java
@@ -0,0 +1,46 @@
+package com.xmpp.listener;
+
+import org.jivesoftware.smack.ConnectionListener;
+
+import com.xmpp.service.MsfService;
+
+public class CheckConnectionListener implements ConnectionListener{
+	
+	private MsfService context;
+	
+	public CheckConnectionListener(MsfService context){
+		this.context=context;
+	}
+
+	@Override
+	public void connectionClosed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void connectionClosedOnError(Exception e) {  
+		if (e.getMessage().equals("stream:error (conflict)")) {
+			
+		}
+	}
+
+	@Override
+	public void reconnectingIn(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reconnectionFailed(Exception arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reconnectionSuccessful() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
